794ae094094510b3aa817f4560433afa4528a351
Karl Frick <karl.frick5@gmail.com>
2021-04-29 22:35:03 -0400

    Created bool statement to cont with a menu or quit

---
+++ PEDAC.md
@@ -0,0 +1,57 @@
+-- Problem --
+For this project, we will model and create a database. We are starting a record label company, and we a place to store our bands, albums, and eventually songs. You are creating a console app that stores our information in a database.
+
+Create a menu system that shows the following options to the user until they choose to quit your program
+
+Add a new band
+View all the bands
+Add an album for a band
+Add a song to an album
+Let a band go (update isSigned to false)
+Resign a band (update isSigned to true)
+Prompt for a band name and view all their albums
+View all albums ordered by ReleaseDate
+View all bands that are signed
+View all bands that are not signed
+Quit the program
+
+-- Example --
+user selects [A]dd...next...[B]and, [A]lbum, [S]ong... user wants to add new band. user must enter the band Name, NumberOfMembers, Website, Style, IsSigned, ContactName, ContactPhoneNumber.
+
+user selects [V]iew options. user will select from view all [B]ands, [A]lbum ordered by release date, [S]igned, [N]ot signed.
+
+user selects [L]et band go....user will type in the bands name to update isSigned to false.
+
+user selects [R]esign band....user will type in the bands name to update isSigned to true.
+
+user selects [S]earch for albums. user will type in the bands name to display all the albums from that band.
+
+user selects [Q]uit. app will end.
+
+-- Data --
+nouns:
+band
+album
+song
+
+behaviors:
+add
+-bands
+-songs
+-albums
+view
+-bands
+-songs
+-albums
+let go
+-boolean
+resign
+-boolean
+search
+-return all the albums for the input band
+quit
+-end application
+
+-- Algorithm --
+
+create Menu:
--- Program.cs
+++ Program.cs
@@ -1,12 +1,89 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace RhythmsGonnaGetYou
 {
-  class Program
-  {
-    static void Main(string[] args)
+    // Define a database context for our Rhythm database.
+    // It derives from (has a parent of) DbContext so we get all the
+    // abilities of a database context from EF Core.
+    class RhythmsGonnaGetYouContext : DbContext
     {
-      Console.WriteLine("Welcome to C#");
+        // Define a Songs property that is a DbSet.
+        public DbSet<Song> Songs { get; set; }
+        // Define a Albums property that is a DbSet.
+        public DbSet<Album> Albums { get; set; }
+        // Define a Bands property that is a DbSet.
+        public DbSet<Band> Bands { get; set; }
+
+        // Define a method required by EF that will configure our connection
+        // to the database.
+        //
+        // DbContextOptionsBuilder is provided to us. We then tell that object
+        // we want to connect to a postgres database named suncoast_movies on
+        // our local machine.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("server=localhost;database=Rhythm");
+        }
     }
-  }
+
+    // create class band to mirror table Bands
+    class Band
+    {
+        // assign a var to the each list of properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CountryOfOrigin { get; set; }
+        public int NumberOfMembers { get; set; }
+        public string Website { get; set; }
+        public string Style { get; set; }
+        public string IsSigned { get; set; }
+        public string ContactName { get; set; }
+        public int ContactPhoneNumber { get; set; }
+    }
+    // create class Album to mirror table Albums
+    class Album
+    {
+        // assign a var to the each list of properties
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string IsExplicit { get; set; }
+        public DateTime ReleaseDate { get; set; } // was "date" in SQL...
+        public int BandId { get; set; }
+
+        // realted list of Bands
+        public Band Band { get; set; }
+
+    }
+    // create class Song to mirror table Songs
+    class Song
+    {
+        // assign a var to the each list of properties
+        public int Id { get; set; }
+        public int TrackNumber { get; set; }
+        public string Title { get; set; }
+        public int Duration { get; set; }
+        public int AlbumId { get; set; }
+
+        // related list of Albums
+        public Album Album { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new RhythmsGonnaGetYouContext();
+
+            // create bool statement to determine if user want to contine with app
+            var keepGoing = true;
+            // if keepGoing = true
+            while (keepGoing)
+            {
+
+            }
+
+        }
+    }
 }
